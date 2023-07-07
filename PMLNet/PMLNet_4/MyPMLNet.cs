eb6d3a6cb66722281faabf9ce20dd6f6c2077472
ShivangKheradiya <shivangatul@gmail.com>
2023-07-07 16:29:40 +0530

    Event is created and event handler is added

---
+++ PMLNet/PMLNet_4/MyPMLNet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Aveva.Core.PMLNet;
+
+namespace PMLNet_4
+{
+    [PMLNetCallable()]
+    public class MyPMLNet
+    {
+        public event PMLNetDelegate.PMLNetEventHandler MyPmlNetEvent;
+
+        [PMLNetCallable()]
+        public MyPMLNet()
+        { }
+
+        [PMLNetCallable()]
+        public void Assign(MyPMLNet that)
+        { }
+
+        [PMLNetCallable()]
+        public void TriggerEvent()
+        {
+            ArrayList args = new ArrayList();
+            args.Add("My Arg 1");
+            args.Add("My Arg 2");
+            if (MyPmlNetEvent != null)
+            {
+                MyPmlNetEvent(args);
+            }
+        }
+
+    }
+}
